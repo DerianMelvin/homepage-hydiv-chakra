3dbe241f01d3c44f5bd4b34fae14b970d561856f
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-06-23 12:55:16 +0800

    Import Header.jsx to App.js - Adjust <div> to use <Box>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Box } from "@chakra-ui/react";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Box>
+      <Header />
+    </Box>
   );
 }
 
