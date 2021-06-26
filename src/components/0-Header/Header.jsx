40ac805dc88253b47ce541a4329f1d0884baab4a
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-06-26 12:06:41 +0800

    Create component Header.jsx - Import component Nav

---
+++ src/components/0-Header/Header.jsx
@@ -0,0 +1,23 @@
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import Nav from "../Nav";
+// import bgImage1 from "../assets/images/background_image_1.jpg";
+
+const Header = () => {
+  return (
+    <Box id="header-container">
+      <Nav />
+
+      <Flex m="5rem" align="center" textAlign="center" justify="center">
+        <Heading as="h1" size="4xl" fontWeight="100">
+          Live with Elegance
+        </Heading>
+      </Flex>
+
+      <Text m="1rem" fontSize="2xl" textAlign="center">
+        Quality furnitures made exclusively for your needs.
+      </Text>
+    </Box>
+  );
+};
+
+export default Header;
