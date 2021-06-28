c08994278cff7a20e8874b7cb9d0de4433a33514
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-06-28 13:11:14 +0800

    Import About to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Box } from "@chakra-ui/react";
 import Header from "./components/0-Header/Header";
+import About from './components/1-About/About'
 
 function App() {
   return (
     <Box>
       <Header />
+      <About />
     </Box>
   );
 }
