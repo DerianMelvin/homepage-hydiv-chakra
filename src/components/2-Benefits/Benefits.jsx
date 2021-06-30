ec1f96ee07bc64af75a762eb15052714fbdf730c
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-06-30 12:56:21 +0800

    Import & add Heading from chakra-ui - Adjust component placement - Adjust some styling properties

--- src/components/2-Benefits/Benefits.jsx
+++ src/components/2-Benefits/Benefits.jsx
@@ -1,25 +1,30 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
 import { MdLocalShipping, MdStar, MdVerifiedUser } from "react-icons/md";
 import ContentBenefits from "./ContentBenefits";
 
 const Benefits = () => {
   return (
-    <Flex px="6%" mt="7rem" align="center" justify="space-between">
-      <ContentBenefits
-        heading="Free Delivery"
-        text="Deliver it straight to your home for purchases available on your region."
-        icon={MdLocalShipping}
-      />
-      <ContentBenefits
-        heading="High Quality"
-        text="Premium materials and textures in every furniture made exclusively for you."
-        icon={MdStar}
-      />
-      <ContentBenefits
-        heading="Warranty"
-        text="Gain at least a 2-year warranty for each furniture you purchased."
-        icon={MdVerifiedUser}
-      />
+    <Flex px="6%" mt="7rem" direction="column" align="center">
+      <Heading as="h2" pb="1rem" mb="4rem" width="100%" size="3xl" textAlign="center" borderBottom="1px solid black">
+        Benefits
+      </Heading>
+      <Flex align="center" justify="space-between">
+        <ContentBenefits
+          heading="Free Delivery"
+          text="Deliver it straight to your home for purchases available on your region."
+          icon={MdLocalShipping}
+        />
+        <ContentBenefits
+          heading="High Quality"
+          text="Premium materials and textures in every furniture made exclusively for you."
+          icon={MdStar}
+        />
+        <ContentBenefits
+          heading="Warranty"
+          text="Gain at least a 2-year warranty for each furniture you purchased."
+          icon={MdVerifiedUser}
+        />
+      </Flex>
     </Flex>
   );
 };
