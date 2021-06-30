a9e1e244302bc3131c3f1a3b1401863ca116d7a5
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-06-30 12:39:54 +0800

    Refactor code to use ContentBenefits component - Import icons from react-icons - Adjust some styling properties

--- src/components/2-Benefits/Benefits.jsx
+++ src/components/2-Benefits/Benefits.jsx
@@ -1,24 +1,25 @@
-import { Flex, Heading, Text, Icon } from "@chakra-ui/react";
-import { MdLocalShipping } from "react-icons/md";
+import { Flex } from "@chakra-ui/react";
+import { MdLocalShipping, MdStar, MdVerifiedUser } from "react-icons/md";
+import ContentBenefits from "./ContentBenefits";
 
 const Benefits = () => {
   return (
-    <Flex align="center" justify="space-evenly">
-      <Flex p="2rem" direction="column" align="center">
-        <Icon as={MdLocalShipping} boxSize="5rem" />
-        <Heading as="h3">Sample Heading</Heading>
-        <Text>This is a sample text</Text>
-      </Flex>
-      <Flex p="2rem" direction="column" align="center">
-        <Icon as={MdLocalShipping} boxSize="5rem" />
-        <Heading as="h3">Sample Heading</Heading>
-        <Text>This is a sample text</Text>
-      </Flex>
-      <Flex p="2rem" direction="column" align="center">
-        <Icon as={MdLocalShipping} boxSize="5rem" />
-        <Heading as="h3">Sample Heading</Heading>
-        <Text>This is a sample text</Text>
-      </Flex>
+    <Flex px="6%" mt="7rem" align="center" justify="space-between">
+      <ContentBenefits
+        heading="Free Delivery"
+        text="Deliver it straight to your home for purchases available on your region."
+        icon={MdLocalShipping}
+      />
+      <ContentBenefits
+        heading="High Quality"
+        text="Premium materials and textures in every furniture made exclusively for you."
+        icon={MdStar}
+      />
+      <ContentBenefits
+        heading="Warranty"
+        text="Gain at least a 2-year warranty for each furniture you purchased."
+        icon={MdVerifiedUser}
+      />
     </Flex>
   );
 };
