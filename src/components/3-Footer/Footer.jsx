cffdced80999271ec9b16cc4a8dd414c9a754f55
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-07-01 12:32:19 +0800

    Refactor code to use LinksFooter - Remove unused imports

--- src/components/3-Footer/Footer.jsx
+++ src/components/3-Footer/Footer.jsx
@@ -1,5 +1,5 @@
-import { Flex, HStack, Link, Image } from "@chakra-ui/react";
-import logo from "../../assets/company_logo.png";
+import { Flex } from "@chakra-ui/react";
+import LinksFooter from "./LinksFooter";
 
 const Footer = () => {
   return (
@@ -12,27 +12,7 @@ const Footer = () => {
       bgColor="whitesmoke"
       zIndex="10"
     >
-      <Flex direction="column" align="flex-start" justify="flex-start">
-        <Image
-          w="auto"
-          h="2rem"
-          objectFit="contain"
-          src={logo}
-          alt="Company logo"
-        />
-
-        <HStack spacing="2">
-          <Link>Home</Link>
-          <Link>Listing</Link>
-          <Link>About Us</Link>
-        </HStack>
-
-        <HStack spacing="2">
-          <Link href="#facebook">Facebook</Link>
-          <Link href="#twitter">Twitter</Link>
-          <Link href="#instagram">Instagram</Link>
-        </HStack>
-      </Flex>
+      <LinksFooter />
     </Flex>
   );
 };
