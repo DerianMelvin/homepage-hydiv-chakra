f3346f39434c26b3d62157670fa119e57e9fcb28
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-07-05 14:53:17 +0800

    Add media queries to About - Import useMediaQuery from chakra-ui - Add textAlign prop to unassigned component - Add mediaLargerThan1200 prop to components

--- src/components/1-About/About.jsx
+++ src/components/1-About/About.jsx
@@ -1,18 +1,22 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useMediaQuery } from "@chakra-ui/react";
 import img1 from "../../assets/images/background_image_1.jpg";
 import img2 from "../../assets/images/background_image_2.jpg";
 import img3 from "../../assets/images/background_image_3.jpg";
 import ContentAbout from "./ContentAbout";
 
 const About = () => {
+  const [isLargerThan1200] = useMediaQuery("(min-width: 1200px)");
+
   return (
     <Box px="6%">
       <ContentAbout
         heading="Look and Feel Comfortable"
         text="Maximize your favourite activities with the best-in-class comfort
             with modern looks."
+        textAlign="right"
         flexDirection="row"
         imageSrc={img1}
+        mediaLargerThan1200={isLargerThan1200}
       />
       <ContentAbout
         heading="Optimize Space with Unique Design"
@@ -20,12 +24,15 @@ const About = () => {
         textAlign="left"
         flexDirection="row-reverse"
         imageSrc={img2}
+        mediaLargerThan1200={isLargerThan1200}
       />
       <ContentAbout
         heading="Variety of Choices for a Perfect Home"
         text="Not a great fit for your home? Simply pick another from our wide range of design variety to satisfy your needs for a perfect living space."
+        textAlign="right"
         flexDirection="row"
         imageSrc={img3}
+        mediaLargerThan1200={isLargerThan1200}
       />
     </Box>
   );
