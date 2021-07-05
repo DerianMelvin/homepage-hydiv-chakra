ddae0b47eb2df3b46d1805bf094ddb5d8222aa5b
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-07-05 15:24:50 +0800

    Adjust ContentBenefits to use media queries - Adjust props - Adjust styling properties

--- src/components/2-Benefits/ContentBenefits.jsx
+++ src/components/2-Benefits/ContentBenefits.jsx
@@ -1,25 +1,33 @@
 import { Flex, Heading, Text, Icon } from "@chakra-ui/react";
 
-const ContentBenefits = ({ heading, text, icon }) => {
+const ContentBenefits = ({ heading, text, icon, mediaLargerThan1024 }) => {
   return (
     <Flex
-      p="2rem"
-      height="20rem"
-      width="25%"
-      direction="column"
+      p={mediaLargerThan1024 ? "2rem" : "3rem"}
+      height={mediaLargerThan1024 ? "23rem" : "15rem"}
+      width={mediaLargerThan1024 ? "30%" : "90%"}
+      direction={mediaLargerThan1024 ? "column" : "row"}
       align="center"
       justify="center"
-      textAlign="center"
       boxShadow="0.2rem 0.5rem 1rem lightGrey"
       borderRadius="0.5rem"
     >
-      <Icon as={icon} mb="0.5rem" boxSize="4rem" />
-      <Heading as="h3" mb="2rem" size="xl">
-        {heading}
-      </Heading>
-      <Text fontSize="lg">
-        {text}
-      </Text>
+      <Icon
+        as={icon}
+        boxSize="4rem"
+        mb={mediaLargerThan1024 ? "0.5rem" : "0"}
+        mr={mediaLargerThan1024 ? "0" : "2rem"}
+      />
+
+      <Flex
+        direction="column"
+        textAlign={mediaLargerThan1024 ? "center" : "left"}
+      >
+        <Heading as="h3" size="lg" mb={mediaLargerThan1024 ? "2rem" : "0.5rem"}>
+          {heading}
+        </Heading>
+        <Text fontSize="lg">{text}</Text>
+      </Flex>
     </Flex>
   );
 };
