bcb9ca151a65dfbb608ebcbe98760b949d251181
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-07-05 14:57:25 +0800

    Add media queries to Header - Import useMediaQuery from chakra-ui - Add mediaLargerThan1200 prop to components

--- src/components/0-Header/Header.jsx
+++ src/components/0-Header/Header.jsx
@@ -1,14 +1,16 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useMediaQuery } from "@chakra-ui/react";
 import BackgroundHeader from "./BackgroundHeader";
 import ContentHeader from "./ContentHeader";
 import Nav from "./Nav";
 
 const Header = () => {
+  const [isLargerThan1200] = useMediaQuery("(min-width: 1200px)");
+
   return (
     <Box id="header-container">
       <Nav />
-      <BackgroundHeader />
-      <ContentHeader />
+      <BackgroundHeader mediaLargerThan1200={isLargerThan1200} />
+      <ContentHeader mediaLargerThan1200={isLargerThan1200} />
     </Box>
   );
 };
